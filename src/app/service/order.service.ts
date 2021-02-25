00aa5f1087af5c4dfe5d4e15850523c03414341d
Zsofi <galambvarizsofia@gmail.com>
2021-02-25 13:06:08 +0100

    order service done

---
+++ src/app/service/order.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Order } from '../model/order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  orderApiUrl: string = 'http://localhost:3000/orders';
+
+  list$: BehaviorSubject<Order[]> = new BehaviorSubject<Order[]>([]);
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAll(): void {
+    this.http.get<Order[]>(this.orderApiUrl).subscribe(
+      orders => this.list$.next(orders)
+    );
+  }
+
+  get(id: number | string): Observable<Order> {
+    id = parseInt(('' + id), 10);
+    return this.http.get<Order>(`${this.orderApiUrl}/${id}`);
+  }
+
+  create(order: Order): void {
+    this.http.post<Order>(this.orderApiUrl, order).subscribe(
+      () => this.getAll()
+    );
+  }
+
+  update(order: Order): void {
+    this.http.patch<Order>(`${this.orderApiUrl}/${order.id}`, order).subscribe(
+      () => this.getAll()
+    );
+  }
+
+  remove(id: number | string ): void {
+    id = parseInt(('' + id), 10);
+    this.http.delete<Order>(`${this.orderApiUrl}/${id}`).subscribe(
+      () => this.getAll()
+    );
+    }
+}
